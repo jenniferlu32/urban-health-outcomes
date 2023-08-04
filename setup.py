7b588be6271ad2ac113443024d0865c4704610d4
Jennifer Lu <jenniferlu32@gmail.com>
2023-08-05 10:57:47 +1200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Assessing the Impact of Urban Development and Pollution on Public Health',
+    author='Jennifer Lu',
+    license='',
+)
